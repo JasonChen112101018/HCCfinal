b1b84be5627d78c94adf9a6de5e57ff35ae12267
JasonChen112101018 <160493897+JasonChen112101018@users.noreply.github.com>
2025-06-11 00:40:44 +0800

    Update and rename tello_mission.launch.py to tello_launch.py

--- tello_launch.py
+++ tello_launch.py
@@ -5,32 +5,32 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='tello_controller_node',
-            executable='tello_controller_node',
+            executable='tello_controller_node_main',
             name='tello_controller',
             output='screen'
         ),
         Node(
             package='yolov11_detector_node',
-            executable='yolov11_detector_node',
+            executable='yolov11_detector_node_main',
             name='yolov11_detector',
             output='screen'
         ),
         Node(
             package='apriltag_detector_node',
-            executable='apriltag_detector_node',
+            executable='apriltag_detector_node_main',
             name='apriltag_detector',
             output='screen'
         ),
         Node(
             package='navigation_node',
-            executable='navigation_node',
+            executable='navigation_node_main',
             name='navigation',
             output='screen'
         ),
         Node(
             package='mission_manager_node',
-            executable='mission_manager_node',
+            executable='mission_manager_node_main',
             name='mission_manager',
             output='screen'
-        ),
+        )
     ])
